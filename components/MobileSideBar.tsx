c70425b0cdf51b74448dff7a097810d88b6f05f5
ReDay Zarra <redayzarra@gmail.com>
2023-11-22 08:36:41 -0500

    Setting up Sidebar

--- components/MobileSideBar.tsx
+++ components/MobileSideBar.tsx
@@ -1,10 +1,7 @@
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
+  SheetTrigger
 } from "@/components/ui/sheet";
 import { CgMenuLeftAlt } from "react-icons/cg";
 import SideBar from "./SideBar";
@@ -15,14 +12,8 @@ const MobileSideBar = () => {
       <SheetTrigger className="md:hidden hover:opacity-75 transition-all">
         <CgMenuLeftAlt size={24} />
       </SheetTrigger>
-      <SheetContent className="w-[400px] sm:w-[540px]">
-        <SheetHeader>
-          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
+      <SheetContent side="left" className="w-60 p-0 shadow-none">
+        <SideBar />
       </SheetContent>
     </Sheet>
   );
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 const SideBar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-background shadow-sm">
-      
+      SideBar
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
