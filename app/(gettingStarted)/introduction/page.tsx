7065cc3e11dee9670d84b798fab912c4cea5e921
ReDay Zarra <redayzarra@gmail.com>
2023-11-28 08:16:18 -0500

    Fixing alignment issues

--- app/(gettingStarted)/introduction/page.tsx
+++ app/(gettingStarted)/introduction/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const IntroductionPage = () => {
   return (
-    <div>IntroductionPage</div>
+    <div className=''>IntroductionPage</div>
   )
 }
 
--- app/(gettingStarted)/layout.tsx
+++ app/(gettingStarted)/layout.tsx
@@ -1,12 +1,13 @@
-import NavBar from "@/components/NavBar";
 import React from "react";
 import { LinkTabs } from "./_components/LinkTabs";
 
 const GettingStartedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <LinkTabs />
-      <main>{children}</main>
+    <div className="flex flex-col items-center">
+      <div className="w-full flex justify-center">
+        <LinkTabs />
+      </div>
+      <main className="w-full mt-5">{children}</main>
     </div>
   );
 };
