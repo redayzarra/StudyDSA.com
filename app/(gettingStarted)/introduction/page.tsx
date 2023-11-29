6fcda77dd285220e556452642d27d82176de4840
ReDay Zarra <redayzarra@gmail.com>
2023-11-29 15:44:53 -0500

    Adding purpose

--- app/(gettingStarted)/introduction/page.tsx
+++ app/(gettingStarted)/introduction/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
 const IntroductionPage = () => {
   return (
-    <div className=''>IntroductionPage</div>
-  )
-}
+    <div className="space-y-6">
+      <h1 className="text-5xl font-black flex items-center justify-center mt-2">
+        Introduction
+      </h1>
+      {/* Welcome paragraph */}
+      <div>
+        <p className="indent-10">
+          <span className="font-bold">Welcome to StudyDSA.com</span>, a personal
+          project born from my own journey and experiences in preparing for job
+          interviews in the tech industry. This website is not just a platform;{" "}
+          <span className="font-bold">it's a sharing space</span> where I bring
+          together the knowledge, strategies, and insights that I've gathered
+          along my path, especially while studying{" "}
+          <span className="font-bold">
+            for Data Structures and Algorithms (DSA)
+          </span>
+          .
+        </p>
+      </div>
 
-export default IntroductionPage
+      <h2 className="text-3xl font-black">Purpose</h2>
+      {/* Purpose paragraph */}
+      <div>
+        <p className="indent-10">
+          <span className="font-bold">StudyDSA.com</span> was born out of my
+          personal experiences and the challenges I faced while preparing for
+          job interviews in the tech industry. This platform is a reflection of
+          my journey, a collection of the lessons, strategies, and insights I've
+          accumulated.{" "}
+          <span className="font-bold">
+            My goal is simple: to share what I've learned in a straightforward,
+            accessible manner
+          </span>
+          . I hope that by doing so, I can make your journey a bit easier,
+          whether you're preparing for interviews or just looking to improve
+          your understanding of these essential concepts.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default IntroductionPage;
