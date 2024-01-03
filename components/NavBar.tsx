82c85b5aec5f32d3a45043bdc4f1ab2ba24b5145
ReDay Zarra <redayzarra@gmail.com>
2024-01-02 19:51:16 -0500

    Wasted time adding a silly glass effect, never again

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,7 +13,7 @@ const NavBar = () => {
     pathname.includes("/login") || pathname.includes("/register");
 
   return (
-    <div className="shadow-md border-b fixed top-0 left-0 right-0 z-50 flex items-center bg-background/50 backdrop-blur-[8px]">
+    <div className="frosty shadow-lg fixed top-0 left-0 right-0 z-50 flex items-center bg-background/60 backdrop-blur-[8px]">
       <div className="mx-auto max-w-7xl py-3 px-4 sm:px-6 lg:px-8 w-full">
         <div className="flex items-center justify-between">
           {/* Mobile Sidebar - Small Screens only */}
