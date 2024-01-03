ea4d6d23b8027070d6a4c05abe7b56752b37ab75
ReDay Zarra <redayzarra@gmail.com>
2024-01-03 16:58:04 -0500

    Creating callbacks and role field

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import { getUserById } from "./data/user";
 import db from "./lib/db";
 
 export const {
@@ -9,6 +10,28 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({token, session}) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role == token.role;
+      }
+
+      return session;
+    },
+    async jwt({token}) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub)
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
