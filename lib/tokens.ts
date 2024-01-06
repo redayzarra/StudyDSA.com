060b36c29d6ce051f3a98d28d49d889744c81cab
ReDay Zarra <redayzarra@gmail.com>
2024-01-06 17:26:41 -0500

    Generating tokens for verification

---
+++ lib/tokens.ts
@@ -0,0 +1,6 @@
+import {v4 as uuidv4} from "uuid";
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000); // 1 hr
+}
---
+++ prisma/migrations/20240106213030_adding_verification_token/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropIndex
+DROP INDEX `User_name_key` ON `user`;
+
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `username` VARCHAR(191) NULL;
+
+-- CreateTable
+CREATE TABLE `VerificationToken` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `VerificationToken_token_key`(`token`),
+    UNIQUE INDEX `VerificationToken_email_token_key`(`email`, `token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_username_key` ON `User`(`username`);
