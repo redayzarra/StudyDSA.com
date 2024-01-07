ad90f2f71b314e4a6d3c7a2c0869b3ceb2659853
ReDay Zarra <redayzarra@gmail.com>
2024-01-07 00:57:35 -0500

    Gaining access to the token from verfication page

--- components/auth/NewVerificationForm.tsx
+++ components/auth/NewVerificationForm.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
+
+import React, { useCallback, useEffect } from "react";
+import CardWrapper from "./CardWrapper";
+import { HashLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
 
 const NewVerificationForm = () => {
-  return (
-    <div>NewVerificationForm</div>
-  )
-}
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
 
-export default NewVerificationForm
+  const onSubmit = useCallback(() => {
+    console.log(token);
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Verify your email"
+      backButtonHref="/login"
+      backButtonLabel="Back to Login"
+    >
+      <div className="flex items-center w-full justify-center animate">
+        <HashLoader color="#f9cb14" />
+      </div>
+    </CardWrapper>
+  );
+};
+
+export default NewVerificationForm;
