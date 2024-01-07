c70687a319ea37fc4cfd3ace3228bd5d29febc8c
ReDay Zarra <redayzarra@gmail.com>
2024-01-07 01:34:32 -0500

    Email verification from verification page works

--- components/auth/NewVerificationForm.tsx
+++ components/auth/NewVerificationForm.tsx
@@ -1,16 +1,41 @@
 "use client";
 
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import CardWrapper from "./CardWrapper";
 import { HashLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/newVerification";
+import FormResult from "./FormResult";
 
 const NewVerificationForm = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
 
+  // Error will determine if I should be destructive, and message
+  // will be a any message I recieve.
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState<string | undefined>("");
+
   const onSubmit = useCallback(() => {
-    console.log(token);
+    if (!token) {
+      setError(true);
+      setMessage("Missing token. Please send a new email");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        if (data.error) {
+          setError(true);
+          setMessage(data.error);
+        } else {
+          setMessage(data.success);
+        }
+      })
+      .catch((error) => {
+        setError(true);
+        setMessage("An error occurred during verification.");
+      });
   }, [token]);
 
   useEffect(() => {
@@ -24,7 +49,8 @@ const NewVerificationForm = () => {
       backButtonLabel="Back to Login"
     >
       <div className="flex items-center w-full justify-center animate">
-        <HashLoader color="#f9cb14" />
+        {!error && <HashLoader color="#f9cb14" />}
+        <FormResult message={message} error={error} />
       </div>
     </CardWrapper>
   );
