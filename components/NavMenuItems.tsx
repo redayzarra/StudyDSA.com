d84c5418deff2f0329dafe5b39ad47c1e12dd6fd
ReDay Zarra <redayzarra@gmail.com>
2024-01-15 21:38:21 -0500

    Adding data structures, algorithms, and machine learning data

--- components/NavMenuItems.tsx
+++ components/NavMenuItems.tsx
@@ -4,49 +4,12 @@ import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import NavListItem from "./NavListItem";
+import { algorithms, dataStructures, machineLearning } from "@/data/navData";
 
-const courses: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
 
 export function NavMenuItems() {
   return (
@@ -59,13 +22,13 @@ export function NavMenuItems() {
           </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {courses.map((course) => (
+              {dataStructures.map((dataStructure) => (
                 <NavListItem
-                  key={course.title}
-                  title={course.title}
-                  href={course.href}
+                  key={dataStructure.title}
+                  title={dataStructure.title}
+                  href={dataStructure.href}
                 >
-                  {course.description}
+                  {dataStructure.description}
                 </NavListItem>
               ))}
             </ul>
@@ -79,13 +42,13 @@ export function NavMenuItems() {
           </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {courses.map((course) => (
+              {algorithms.map((algorithm) => (
                 <NavListItem
-                  key={course.title}
-                  title={course.title}
-                  href={course.href}
+                  key={algorithm.title}
+                  title={algorithm.title}
+                  href={algorithm.href}
                 >
-                  {course.description}
+                  {algorithm.description}
                 </NavListItem>
               ))}
             </ul>
@@ -99,13 +62,13 @@ export function NavMenuItems() {
           </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {courses.map((course) => (
+              {machineLearning.map((ml) => (
                 <NavListItem
-                  key={course.title}
-                  title={course.title}
-                  href={course.href}
+                  key={ml.title}
+                  title={ml.title}
+                  href={ml.href}
                 >
-                  {course.description}
+                  {ml.description}
                 </NavListItem>
               ))}
             </ul>
