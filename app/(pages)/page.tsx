abdc03a8fe5302f0485b2345afe4feb957f45205
ReDay Zarra <redayzarra@gmail.com>
2024-01-16 19:10:48 -0500

    Trying to add copywriting to the landing page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
     <div>
-      
+      <div className="flex mt-24">
+        <div className="flex flex-col space-y-2">
+          <h1 className="text-4xl font-bold">
+            Master Data Structures <br></br> and Algorithms
+          </h1>
+          <h2 className="text-muted-foreground max-w-lg">
+            Everything you need to ace your coding interviews. No more expensive
+            courses. No monthly subsciptions. No ads. No bullshit.
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
