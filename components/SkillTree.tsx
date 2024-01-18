4d2063942ee093ac6c94e8f9013959a03361af0b
ReDay Zarra <redayzarra@gmail.com>
2024-01-18 12:57:31 -0500

    Finishing up skill tree styles

--- components/SkillTree.tsx
+++ components/SkillTree.tsx
@@ -8,16 +8,17 @@ import {
 import { TbBinaryTree } from "react-icons/tb";
 import { PiTreeStructure } from "react-icons/pi";
 import { Badge } from "./ui/badge";
+import { AiOutlineFunction } from "react-icons/ai";
 
 const SkillTree = () => {
   return (
     <div className="w-full rounded-md bg-slate-300/50 dark:bg-zinc-950/50 px-8 py-4">
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
+      <div className="grid gap-8 grid-cols-1 md:grid-cols-2">
         <Accordion type="single" collapsible defaultValue="item-1">
           <AccordionItem value="item-1">
             <AccordionTrigger>
               <div className="flex space-x-4 items-center justify-center">
-                <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex">
+                <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex flex-shrink-0">
                   <TbBinaryTree size={30} />
                 </div>
                 <h1 className="text-xl">Data Structures</h1>
@@ -28,17 +29,31 @@ const SkillTree = () => {
             </AccordionContent>
           </AccordionItem>
         </Accordion>
-        <Accordion type="single" disabled>
-          <AccordionItem value="machineLearning">
-            <AccordionTrigger className="hover:font-medium">
+        {/* Algorithms & Machine Learning */}
+        <Accordion type="single" collapsible defaultValue="algorithms">
+          <AccordionItem value="algorithms">
+            <AccordionTrigger>
               <div className="flex space-x-4 items-center justify-center">
-                <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex">
+                <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex flex-shrink-0">
+                  <AiOutlineFunction size={30} />
+                </div>
+                <h1 className="text-xl">Algorithms</h1>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem disabled value="machineLearning">
+            <AccordionTrigger className="hover:font-medium text-muted-foreground">
+              <div className="flex space-x-4 items-center justify-center">
+                <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex flex-shrink-0">
                   <PiTreeStructure size={30} />
                 </div>
-                <h1 className="text-xl text-muted-foreground">
-                  Machine Learning
-                </h1>
-                <Badge variant="secondary">Coming Soon</Badge>
+                <h1 className="text-xl">Machine Learning</h1>
+                <Badge variant="secondary" className="text-muted-foreground">
+                  Coming Soon
+                </Badge>
               </div>
             </AccordionTrigger>
             <AccordionContent>
