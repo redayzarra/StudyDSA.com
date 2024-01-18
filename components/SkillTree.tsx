9c7c0269199ec9899b669c5384af713a1cc520a5
ReDay Zarra <redayzarra@gmail.com>
2024-01-18 11:13:49 -0500

    Adding accordions to build skill tree

--- components/SkillTree.tsx
+++ components/SkillTree.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const SkillTree = () => {
   return (
-    <div>SkillTree</div>
-  )
-}
+    <div className="w-full rounded-md bg-slate-300/50 dark:bg-zinc-950/50 px-8 py-4">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
+        <Accordion type="single" collapsible defaultValue="open">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
 
-export default SkillTree
+export default SkillTree;
