a4c9c459e7fb6f37795a500ac7524d49e6918742
ReDay Zarra <redayzarra@gmail.com>
2024-01-21 21:07:54 -0500

    Setting up layout for tomorrow

--- components/SkillTree.tsx
+++ components/SkillTree.tsx
@@ -6,18 +6,20 @@ import {
 } from "@/components/ui/accordion";
 import { dataStructures } from "@/data/navData";
 import { AiOutlineFunction } from "react-icons/ai";
-import { TbBinaryTree } from "react-icons/tb";
+import { MdDataArray } from "react-icons/md";
+import { AiOutlineNodeIndex } from "react-icons/ai";
 
 const SkillTree = () => {
   return (
     <div className="w-full shadow-lg rounded-md bg-slate-200/50 dark:bg-zinc-950/50 border-t-2 border-white dark:border-border px-8 py-4">
       <div className="grid gap-8 grid-cols-1 xm:grid-cols-2">
+        {/* Arrays */}
         <Accordion type="single" collapsible defaultValue="item-1">
           <AccordionItem value="item-1">
             <AccordionTrigger>
               <div className="flex space-x-4 items-center justify-center">
                 <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex flex-shrink-0">
-                  <TbBinaryTree size={30} />
+                  <MdDataArray size={30} />
                 </div>
                 <h1 className="text-xl">Arrays</h1>
               </div>
@@ -31,22 +33,22 @@ const SkillTree = () => {
             </AccordionContent>
           </AccordionItem>
         </Accordion>
-        {/* Algorithms & Machine Learning */}
+        {/* Linked Lists */}
         <Accordion type="single" collapsible defaultValue="algorithms">
           <AccordionItem value="algorithms">
             <AccordionTrigger>
               <div className="flex space-x-4 items-center justify-center">
                 <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex flex-shrink-0">
-                  <AiOutlineFunction size={30} />
+                  <AiOutlineNodeIndex size={30} />
                 </div>
-                <h1 className="text-xl">Algorithms</h1>
+                <h1 className="text-xl">Linked Lists</h1>
               </div>
             </AccordionTrigger>
             <AccordionContent>
               <div className="">
                 <ul className="grid grid-cols-2 gap-4">
                   {dataStructures.map((dataStructure) => (
-                    <div className="rounded-sm p-2 bg-transparent hover:bg-muted-foreground/10 dark:hover:bg-black/25 hover:cursor-pointer space-y-2">
+                    <div className="rounded-sm transition-all p-2 bg-transparent hover:bg-muted-foreground/10 dark:hover:bg-black/25 hover:cursor-pointer space-y-2">
                       <h1 className="font-semibold">{dataStructure.title}</h1>
                       <h2 className="line-clamp-2">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
