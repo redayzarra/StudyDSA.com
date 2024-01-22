0258e307c06c59a1fd3c133dc4b2d5240f065e77
ReDay Zarra <redayzarra@gmail.com>
2024-01-22 10:36:29 -0500

    Testing accordion

--- components/DataStructureSkills.tsx
+++ components/DataStructureSkills.tsx
@@ -15,9 +15,9 @@ const DataStructureSkills = () => {
     <div className="w-full shadow-lg rounded-md bg-slate-200/50 dark:bg-zinc-950/50 border-t-2 border-white dark:border-border px-8 py-4">
       <SkillTreeHeading>Data Structures</SkillTreeHeading>
       <div className="grid gap-x-8 grid-cols-1 xm:grid-cols-2">
-        {/* Arrays */}
-        <Accordion type="single" collapsible defaultValue="algorithms">
-          <AccordionItem value="algorithms">
+        <Accordion type="single" collapsible>
+          {/* Arrays */}
+          <AccordionItem value="arrays">
             <AccordionTrigger>
               <div className="flex space-x-4 items-center justify-center">
                 <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex flex-shrink-0">
@@ -41,9 +41,33 @@ const DataStructureSkills = () => {
               </div>
             </AccordionContent>
           </AccordionItem>
+          <AccordionItem value="linkedLists">
+            <AccordionTrigger>
+              <div className="flex space-x-4 items-center justify-center">
+                <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex flex-shrink-0">
+                  <AiOutlineNodeIndex size={30} />
+                </div>
+                <h1 className="text-xl">Linked Lists</h1>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="">
+                <ul className="grid grid-cols-2 gap-4">
+                  {dataStructures.map((dataStructure) => (
+                    <div className="rounded-sm transition-all p-2 bg-transparent hover:bg-muted-foreground/10 dark:hover:bg-black/25 hover:cursor-pointer space-y-2">
+                      <h1 className="font-semibold">{dataStructure.title}</h1>
+                      <h2 className="line-clamp-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      </h2>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
         </Accordion>
         {/* Hashmaps */}
-        <Accordion type="single" collapsible defaultValue="algorithms">
+        <Accordion type="single" collapsible>
           <AccordionItem value="algorithms">
             <AccordionTrigger>
               <div className="flex space-x-4 items-center justify-center">
@@ -70,32 +94,7 @@ const DataStructureSkills = () => {
           </AccordionItem>
         </Accordion>
         {/* Linked Lists */}
-        <Accordion type="single" collapsible defaultValue="algorithms">
-          <AccordionItem value="algorithms">
-            <AccordionTrigger>
-              <div className="flex space-x-4 items-center justify-center">
-                <div className="rounded-full bg-background/50 dark:bg-foreground/10 shadow-md h-12 w-12 items-center justify-center flex flex-shrink-0">
-                  <AiOutlineNodeIndex size={30} />
-                </div>
-                <h1 className="text-xl">Linked Lists</h1>
-              </div>
-            </AccordionTrigger>
-            <AccordionContent>
-              <div className="">
-                <ul className="grid grid-cols-2 gap-4">
-                  {dataStructures.map((dataStructure) => (
-                    <div className="rounded-sm transition-all p-2 bg-transparent hover:bg-muted-foreground/10 dark:hover:bg-black/25 hover:cursor-pointer space-y-2">
-                      <h1 className="font-semibold">{dataStructure.title}</h1>
-                      <h2 className="line-clamp-2">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      </h2>
-                    </div>
-                  ))}
-                </ul>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+        <Accordion type="single" collapsible></Accordion>
       </div>
     </div>
   );
