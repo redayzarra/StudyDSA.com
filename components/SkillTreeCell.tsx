f4d1fa0c775d208e6a7c3789c88ad01d90e39dbd
ReDay Zarra <redayzarra@gmail.com>
2024-01-24 18:46:00 -0500

    Adding checkbox to skill tree cells

--- components/SkillTreeCell.tsx
+++ components/SkillTreeCell.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
+import { Checkbox } from "./ui/checkbox";
 
 interface Props {
   description?: string;
@@ -12,12 +13,15 @@ const SkillTreeCell = ({
   description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, sapiente?",
 }: PropsWithChildren<Props>) => {
   return (
-    <Link href={href}>
-      <div className="rounded-sm transition-all p-2 bg-transparent hover:bg-muted-foreground/10 dark:hover:bg-black/25 hover:cursor-pointer space-y-2">
-        <h1 className="font-semibold">{children}</h1>
-        <h2 className="line-clamp-2 text-muted-foreground">{description}</h2>
-      </div>
-    </Link>
+    <div className="relative">
+      <Link href={href}>
+        <div className="rounded-sm transition-all p-2 bg-transparent hover:bg-muted-foreground/10 dark:hover:bg-black/25 hover:cursor-pointer space-y-2">
+          <h1 className="font-semibold">{children}</h1>
+          <h2 className="line-clamp-2 text-muted-foreground">{description}</h2>
+        </div>
+      </Link>
+      <Checkbox className="absolute top-2 right-2" />
+    </div>
   );
 };
 
