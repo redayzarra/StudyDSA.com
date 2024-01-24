fb36f11a7dbcb08556f25414636588ad4a559a8d
ReDay Zarra <redayzarra@gmail.com>
2024-01-24 18:09:50 -0500

    Adding data for linked lists

--- data/skillsData.ts
+++ data/skillsData.ts
@@ -5,6 +5,11 @@ export const arraySkills: { title: string; href: string; description: string }[]
     description: "You just store stuff in a line, it's quite literally a list of stuff",
   },
   {
+    title: "Algorithms",
+    href: "/",
+    description: "These are common algorithms you need to know for arrays, stacks, and strings",
+  },
+  {
     title: "Operations",
     href: "/",
     description: "Here's everything you can do with arrays and their complexities",
@@ -30,14 +35,9 @@ export const arraySkills: { title: string; href: string; description: string }[]
     description: "LIFO - meaning you can quickly access the most recent one",
   },
   {
-    title: "Applications",
-    href: "/",
-    description: "You should use arrays for storing the same type of things",
-  },
-  {
     title: "Best Practices",
     href: "/",
-    description: "The most efficient way to use arrays and avoid pitfalls",
+    description: "The most efficient way of using arrays and avoiding pitfalls",
   },
 ]
 
@@ -45,7 +45,12 @@ export const linkedSkills: { title: string; href: string; description: string }[
   {
     title: "Definition",
     href: "/",
-    description: "It's like that Spider-Man meme where they are pointing to each other",
+    description: "It's like that Spider-Man meme where they are all pointing to each other",
+  },
+  {
+    title: "Algorithms",
+    href: "/",
+    description: "These are common algorithms you need to know for linked lists",
   },
   {
     title: "Operations",
@@ -55,32 +60,27 @@ export const linkedSkills: { title: string; href: string; description: string }[
   {
     title: "Implementation",
     href: "/",
-    description: "They point to stuff instead of storing data",
+    description: "You implement a linked list with a 'Node' class that has pointers and values",
   },
   {
     title: "Pointers",
     href: "/",
-    description: "They're like a metal can, they can only fit a certain amount",
+    description: "Nodes have pointers which point to the previous or next node",
   },
   {
     title: "Singly Linked List",
     href: "/",
-    description: "They're basically balloons, you can keep adding to them with no worries",
+    description: "Think of a treasure hunt, each clue leads to the next until the end",
   },
   {
     title: "Doubly Linked List",
     href: "/",
-    description: "They're basically balloons, you can keep adding to them with no worries",
+    description: "Think of this as a two-way street, you can go to previous or next node",
   },
   {
-    title: "Circular Linked List",
+    title: "Best Practices",
     href: "/",
-    description: "They're basically balloons, you can keep adding to them with no worries",
-  },
-  {
-    title: "Applications",
-    href: "/",
-    description: "Whenever you want to add or remove to the beginning or the end",
+    description: "The most efficient way of using linked lists and avoiding pitfalls",
   },
 ]
 
