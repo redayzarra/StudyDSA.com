07708e2cbf31c0719eb1aba1cf90d7dfec22d2a1
ReDay Zarra <redayzarra@gmail.com>
2024-02-07 15:34:36 -0500

    Creating algorithm cell

---
+++ components/AlgorithmCell.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AlgorithmCell = () => {
+  return (
+    <div className="rounded-md bg-white">
+      <h1>Sorting</h1>
+    </div>
+  )
+}
+
+export default AlgorithmCell
--- components/Basics.tsx
+++ components/Basics.tsx
@@ -17,6 +17,7 @@ import { TbBinaryTree, TbBinaryTree2, TbVector } from "react-icons/tb";
 import SkillTreeHeading from "./SkillTreeHeading";
 import SkillTreeItem from "./SkillTreeItem";
 import { PiGraph } from "react-icons/pi";
+import AlgorithmCell from "./AlgorithmCell";
 
 const Basics = () => {
   return (
@@ -57,33 +58,7 @@ const Basics = () => {
         </Accordion>
       </div>
 
-      {/* Intermediate Data Structrues */}
-      <h2 className="font-semibold mt-10 mb-2">Algorithms</h2>
-      <div className="grid gap-x-8 grid-cols-1 xm:grid-cols-2">
-        {/* First Column */}
-        <Accordion type="multiple">
-          {/* Trees */}
-          <SkillTreeItem items={treeSkills} name="Trees" value="trees">
-            <TbBinaryTree size={30} />
-          </SkillTreeItem>
-          {/* Graphs */}
-          <SkillTreeItem items={graphSkills} name="Graphs" value="graphs">
-            <TbVector size={28} />
-          </SkillTreeItem>
-        </Accordion>
-
-        {/* Second Column */}
-        <Accordion type="multiple">
-          {/* Heaps */}
-          <SkillTreeItem name="Heaps" value="heaps" items={heapSkills}>
-            <FaArrowUpRightDots size={26} />
-          </SkillTreeItem>
-          {/* Tries */}
-          <SkillTreeItem name="Tries" value="tries" items={trieSkills}>
-            <TbBinaryTree2 size={28} />
-          </SkillTreeItem>
-        </Accordion>
-      </div>
+      <AlgorithmCell />
     </div>
   );
 };
