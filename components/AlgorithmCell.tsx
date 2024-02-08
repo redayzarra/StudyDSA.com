20acc6ad800ccde68c67da13719e08837a0569a4
ReDay Zarra <redayzarra@gmail.com>
2024-02-08 13:36:26 -0500

    Styling algorithms cell

--- components/AlgorithmCell.tsx
+++ components/AlgorithmCell.tsx
@@ -1,19 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
-import { Checkbox } from './ui/checkbox'
+import Link from "next/link";
+import React from "react";
+import { Checkbox } from "./ui/checkbox";
 
 const AlgorithmCell = () => {
   return (
-    <div className="relative">
+    <div className="relative hover:-translate-y-1 transition-all">
       <Link href="/">
-        <div className="rounded-sm shadow-md transition-all p-2 bg-primary/25 hover:bg-muted-foreground/10 dark:hover:bg-black/25 border-t-2 border-white dark:border-white/10 hover:cursor-pointer space-y-2">
-          <h1 className="font-semibold">Sorting</h1>
-          <h2 className="line-clamp-2 text-muted-foreground">Sorting is how you reorder a list</h2>
+        <div className="rounded-sm shadow-md transition-all p-2 bg-gray-300/50 hover:bg-gray-300/70 dark:bg-muted-foreground/10 dark:hover:bg-muted-foreground/20 border-t-2 border-white dark:border-white/10 hover:cursor-pointer space-y-2">
+          <h1 className="font-semibold text-[1rem]">Sorting</h1>
+          <h2 className="line-clamp-1 text-muted-foreground text-sm">
+            Sorting is how you reorder a list
+          </h2>
         </div>
       </Link>
       <Checkbox className="absolute top-2 right-2 rounded-full" />
     </div>
-  )
-}
+  );
+};
 
-export default AlgorithmCell
+export default AlgorithmCell;
