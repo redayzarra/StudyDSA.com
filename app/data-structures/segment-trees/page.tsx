b920f0eab83720b715077a67eb4e86bc8087e785
ReDay Zarra <redayzarra@gmail.com>
2024-04-02 11:06:09 -0400

    Starting segment trees page

---
+++ app/data-structures/segment-trees/page.tsx
@@ -0,0 +1,152 @@
+import getAlgorithmsByName from "@/actions/algorithms/getAlgorithmsByName";
+import findChapter from "@/actions/chapters/findChapter";
+import getTopicByName from "@/actions/topics/getTopicByName";
+import Algorithms from "@/components/Algorithms";
+import ChapterHeading from "@/components/ChapterHeading";
+import CodeBlock from "@/components/CodeBlock";
+import { CodeText } from "@/components/CodeText";
+import Heading from "@/components/Heading";
+import Operations from "@/components/Operations";
+import { trieOperations } from "@/data/operationsData";
+import { Metadata } from "next";
+import ImageBlock from "@/components/ImageBlock";
+import TextLink from "@/components/TextLink";
+
+const SegmentTreesPage = async () => {
+  const topic = await getTopicByName("Segment Trees");
+
+  // I will have to change this later
+  if (!topic) {
+    console.error("Topic not found");
+    return <div>Topic not found</div>;
+  }
+
+  const definitionChapter = findChapter(topic, "Definition");
+  const operationsChapter = findChapter(topic, "Operations");
+  const disjointSetsChapter = findChapter(topic, "Disjoint Sets");
+  const networkChapter = findChapter(topic, "Network Connectivity");
+  const pathChapter = findChapter(topic, "Path Compression");
+  const rankChapter = findChapter(topic, "Union by Rank");
+  const implementationChapter = findChapter(topic, "Implementation");
+  const bestPracticesChapter = findChapter(topic, "Best Practices");
+
+  const fetchAlgorithms = [
+    "Intervals",
+    "Depth-First Search",
+    "Breadth-First Search",
+    "1D Dynamic Pro.",
+  ];
+
+  const segmentTreesAlgorithms = await getAlgorithmsByName(fetchAlgorithms);
+
+  return (
+    <div className="space-y-8">
+      <Heading topic={topic!} />
+      <ChapterHeading
+        id="definition"
+        title="Definition"
+        chapterId={definitionChapter?.id}
+      >
+        <p></p>
+      </ChapterHeading>
+
+      <ChapterHeading
+        id="operations"
+        title="Operations"
+        chapterId={operationsChapter?.id}
+      ></ChapterHeading>
+
+      <ChapterHeading
+        id="disjoint-sets"
+        title="Disjoint Sets"
+        chapter={disjointSetsChapter}
+        chapterId={disjointSetsChapter?.id}
+      >
+        <p></p>
+      </ChapterHeading>
+
+      <ChapterHeading
+        id="connectivity"
+        title="Network Connectivity"
+        chapter={pathChapter}
+        chapterId={pathChapter?.id}
+      ></ChapterHeading>
+
+      <ChapterHeading
+        id="path-compression"
+        title="Path Compression"
+        chapter={networkChapter}
+        chapterId={networkChapter?.id}
+      >
+        <p></p>
+      </ChapterHeading>
+
+      <ChapterHeading
+        id="union-by-rank"
+        title="Union by Rank"
+        chapter={rankChapter}
+        chapterId={rankChapter?.id}
+      ></ChapterHeading>
+
+      <ChapterHeading
+        id="implementation"
+        title="Implementation"
+        chapterId={implementationChapter?.id}
+      ></ChapterHeading>
+
+      <ChapterHeading id="algorithms" title="Algorithms">
+        <Algorithms items={segmentTreesAlgorithms} />
+      </ChapterHeading>
+
+      <ChapterHeading
+        id="bestPractices"
+        title="Best Practices"
+        chapterId={bestPracticesChapter?.id}
+      >
+        <p>
+          Tries efficiently store and search words or sequences of strings. They
+          are useful for tasks involving prefixes, such as autocomplete systems
+          or spell checkers. To get the most out of tries in coding interviews,
+          here are some essential tips and tricks:
+        </p>
+        <br />
+        <ul className="ml-6">
+          <li>
+            <span className="font-bold">&bull; Use Hash Maps:</span> When
+            dealing with a large alphabet, use hash maps to store children nodes
+            instead of fixed-size arrays. This improves space efficiency and
+            flexibility, allowing the trie to handle any range of characters.
+          </li>
+          <br />
+          <li>
+            <span className="font-bold">
+              &bull; Iterative vs. Recursive Implementations:
+            </span>{" "}
+            Be comfortable with both iterative and recursive implementations of
+            trie operations. Recursive approaches are often more intuitive but
+            can be inefficient with very deep tries. Iterative implementations,
+            while sometimes more complex, can be more efficient.
+          </li>
+          <br />
+          <li>
+            <span className="font-bold">
+              &bull; Handling Deletions Carefully:
+            </span>{" "}
+            Implement deletion operations carefully to ensure the trie remains
+            consistent. After removing a word, check whether the parent nodes
+            have become unnecessary (i.e., they no longer have any children and
+            are not end of any word) and remove them as well.
+          </li>
+          <br />
+        </ul>
+      </ChapterHeading>
+    </div>
+  );
+};
+
+export const metadata: Metadata = {
+  title: "Union-Find",
+  description: "",
+};
+
+export default SegmentTreesPage;
