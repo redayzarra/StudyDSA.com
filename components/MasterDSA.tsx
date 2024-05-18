30300e2e34779ed7294405bcefa184f982b566da
ReDay Zarra <redayzarra@gmail.com>
2024-05-18 02:42:49 -0400

    Adding flip words to MasterDSA

--- components/MasterDSA.tsx
+++ components/MasterDSA.tsx
@@ -3,6 +3,7 @@ import { TextGenerateEffect } from "./ui/generate-text";
 import { Spotlight } from "./ui/Spotlight";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { FlipWords } from "./ui/FlipWords";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -10,20 +11,22 @@ const font = Poppins({
 });
 
 const MasterDSA = () => {
+  const words = ["Study", "Master", "Learn", "Explore"];
+
   return (
     <div className="flex flex-col space-y-4 relative">
       {/* <Spotlight className="hidden dark:block top-[-30rem] left-[-25rem] overflow-hidden h-[700%] w-[180vh]" /> */}
       <h1
         className={cn(
-          "text-4xl pb-2 text-center md:text-left md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-700 to-neutral-900 dark:from-neutral-50 dark:to-neutral-400 bg-opacity-50",
+          "text-4xl pb-2 text-center md:text-left md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50",
           font.className
         )}
       >
-        Master Data Structures and Algorithms
+        <FlipWords words={words} className="" /> Data Structures and Algorithms
       </h1>
       <p className="text-neutral-900 dark:text-muted-foreground text-center md:text-left">
-        Let&apos;s make the tough stuff easy. Get straightforward, free resources to
-        tackle data structures and algorithms.
+        Let&apos;s make the tough stuff easy. Get straightforward, free
+        resources to tackle data structures and algorithms.
       </p>
       <TextGenerateEffect
         className="italic font-medium hidden md:flex"
