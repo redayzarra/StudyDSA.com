0f723c2455d2c0a7a27b65d641cb1273e9ce1e45
ReDay Zarra <redayzarra@gmail.com>
2024-05-18 17:49:52 -0400

    Removing NavBar items for now

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,33 +7,34 @@ import MobileSideBar from "./MobileSideBar";
 import { ModeToggle } from "./ModeToggle";
 import UserButton from "./auth/UserButton";
 import { NavMenuItems } from "./NavMenuItems";
+import { VanishInput } from "./ui/VanishInput";
 
 const NavBar = () => {
   const pathname = usePathname();
   const authRoute = authRoutes.includes(pathname);
 
+  const placeholders = [
+    "Arrays",
+    "Hashmaps",
+    "Recursion",
+    "Dynamic Programming",
+    "Graphs",
+  ];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   return (
     <div className="shadow-md border-b fixed top-0 left-0 right-0 z-50 flex items-center bg-slate-200/50 dark:bg-zinc-950/70 backdrop-blur-[8px] min-h-[69px]">
       <div className="mx-auto max-w-[85rem] py-3 px-4 sm:px-6 lg:px-8 w-full">
         <div className="flex items-center justify-between">
-          {/* Mobile Sidebar - Small Screens only */}
-          <div className="block md:hidden">
-            <MobileSideBar />
-          </div>
+          <Logo />
 
-          <div className="hidden md:flex">
-            <Logo />
-          </div>
-
-          <div className="block md:hidden absolute left-1/2 transform -translate-x-1/2">
-            <Logo />
-          </div>
-
-          <div className="hidden md:flex absolute left-1/2 transform -translate-x-1/2">
-            <NavMenuItems />
-          </div>
-
-          {/* Dark Mode and LogIn */}
           <div className="flex gap-x-2 items-center justify-center">
             {!authRoute && (
               <>
