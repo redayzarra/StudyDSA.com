567860f41cce41b973c02a77e452c13eb25e14bb
ReDay Zarra <redayzarra@gmail.com>
2024-05-24 07:18:50 -0400

    Adding appropriately sized table

--- app/practice/page.tsx
+++ app/practice/page.tsx
@@ -1,22 +1,17 @@
-import CodeBlock from "@/components/CodeBlock";
+import Container from "@/components/Container";
 import NavBar from "@/components/NavBar";
-import NotationsChart from "@/components/NotationsChart";
+import { QuestionsTable } from "@/components/QuestionsTable";
+import { Aurora } from "@/components/ui/Aurora";
 
 const PracticePage = () => {
-  const helloCode = `def welcome(is_new):
-      if is_new:
-          return "Hello!"
-      return "Welcome Back!"`;
-  
   return (
-    <div className="flex w-full relative bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-background via-muted-foreground/10 to-muted-foreground/[0.22] dark:bg-neutral-950/[0.96] dark:bg-dot-white/[0.05]">
-      <NavBar />
-      <NotationsChart />
-      <CodeBlock
-          code={helloCode}
-          language="python"
-          title="HarderExample.py"
-        />
+    <div>
+      <Aurora>
+        <NavBar />
+        <Container className="absolute mt-36 md:mt-48">
+          <QuestionsTable />
+        </Container>
+      </Aurora>
     </div>
   );
 };
--- components/Container.tsx
+++ components/Container.tsx
@@ -1,8 +1,12 @@
 import React, { PropsWithChildren } from "react";
 
-const Container = ({ children }: PropsWithChildren) => {
+interface Props {
+  className?: string;
+}
+
+const Container = ({ children, className}: PropsWithChildren<Props>) => {
   return (
-    <div className="mx-auto max-w-[85rem] py-6 px-4 sm:px-6 lg:px-8 w-full">
+    <div className={`mx-auto max-w-[85rem] py-6 px-4 sm:px-6 lg:px-8 w-full ${className}`}>
       {children}
     </div>
   );
