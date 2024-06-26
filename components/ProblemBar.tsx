a5f15142cf2f977a91ab3af3f19615ea171ab521
ReDay Zarra <redayzarra@gmail.com>
2024-06-25 20:05:01 -0400

    Setting up problem bar

--- components/ProblemBar.tsx
+++ components/ProblemBar.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { ProblemSearchBar } from "./ProblemSearchBar";
 
 const ProblemBar = () => {
   return (
-    <div>ProblemBar</div>
-  )
-}
+    <div>
+      <div className="w-full border-dashed backdrop-blur-[15px] border-2 shadow-2xl shadow-black rounded-md bg-black/[.35] border-neutral-800/[.35] p-2">
+        {/* <div className="absolute inset-x-0 h-[1px] mx-auto -top-px bg-gradient-to-r from-transparent via-stone-400 to-transparent" /> */}
+        <ProblemSearchBar />
+      </div>
+    </div>
+  );
+};
 
-export default ProblemBar
+export default ProblemBar;
--- components/ProblemSearchBar.tsx
+++ components/ProblemSearchBar.tsx
@@ -38,19 +38,16 @@ export function ProblemSearchBar({ ...props }: DialogProps) {
       <Button
         variant="outline"
         className={cn(
-          "relative h-8 w-full justify-start rounded-[0.5rem] bg-black hover:bg-neutral-950 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40"
+          "relative h-8 w-full justify-start rounded-[0.5rem] bg-black hover:bg-neutral-950 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-44"
         )}
         onClick={() => setOpen(true)}
         {...props}
       >
-        <FaSearch className="mr-2" />
-        <span>Search...</span>
-        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-          <span className="text-xs">⌘</span>K
-        </kbd>
+        <FaSearch className="mr-2 absolute" />
+        <span className="ml-6">Search problems...</span>
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Search documentation..." />
+        <CommandInput placeholder="Search all problems..." />
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup heading="Links">
