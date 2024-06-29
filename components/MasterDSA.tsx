8e2f9d87a110ffa7012984fcda3d1318e8c4c5ba
ReDay Zarra <redayzarra@gmail.com>
2024-06-29 19:49:02 -0400

    Adding wrap balancer for hero section

--- components/MasterDSA.tsx
+++ components/MasterDSA.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TextGenerateEffect } from "./ui/generate-text";
-import { Spotlight } from "./ui/Spotlight";
+import Balancer from "react-wrap-balancer";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { FlipWords } from "./ui/FlipWords";
@@ -16,18 +16,23 @@ const MasterDSA = () => {
   return (
     <div className="flex flex-col space-y-4 relative">
       {/* <Spotlight className="hidden dark:block top-[-30rem] left-[-25rem] overflow-hidden h-[700%] w-[180vh]" /> */}
-      <h1
-        className={cn(
-          "text-5xl pb-2 text-left md:text-4xl lg:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 transition-all duration-700",
-          font.className
-        )}
-      >
-        <FlipWords words={words} className="" /> Data <br/> Structures & Algorithms
-      </h1>
-      <p className="text-muted-foreground text-left">
-        Let&apos;s make the tough stuff easy. Get straightforward, free
-        resources to tackle data structures and algorithms.
-      </p>
+      <Balancer>
+        <h1
+          className={cn(
+            "text-5xl pb-2 text-left md:text-4xl lg:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 transition-all duration-700",
+            font.className
+          )}
+        >
+          <FlipWords words={words} className="" /> Data <br /> Structures &
+          Algorithms
+        </h1>
+      </Balancer>
+      <Balancer>
+        <p className="text-muted-foreground text-left">
+          Let&apos;s make the tough stuff easy. Get straightforward, free
+          resources to tackle data structures and algorithms.
+        </p>
+      </Balancer>
       <TextGenerateEffect
         className="italic font-medium"
         words={`"Where complexity meets clarity."`}
