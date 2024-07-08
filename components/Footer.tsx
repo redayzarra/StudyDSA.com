cd39c40e64d056cbc61a475b0e20fc7baa7198bc
ReDay Zarra <redayzarra@gmail.com>
2024-07-08 07:58:52 -0400

    Fixing aurora problem

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import SocialLinks from "./SocialLinks";
 
 const Footer = () => {
   return (
-    <div className="h-[100px] hidden md:flex flex-col space-y-4 bg-transparent border-t border-1 items-center justify-center">
+    <div className="h-[100px] flex flex-col space-y-4 bg-transparent border-t border-1 items-center justify-center">
       <SocialLinks size={26} className="mt-2" />
       <p className="text-neutral-500 text-[12px] font-normal">
         Copyright © StudyDSA. All rights reserved.
