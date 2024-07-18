1515dcf6489375849ec30f58c7499ff61c6a7d97
ReDay Zarra <redayzarra@gmail.com>
2024-07-18 04:32:24 -0400

    Don't allow dangerous auth

--- auth.config.ts
+++ auth.config.ts
@@ -12,7 +12,7 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
+      allowDangerousEmailAccountLinking: false,
     }),
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
