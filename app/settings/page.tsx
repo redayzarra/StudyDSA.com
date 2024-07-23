f3990f0aebfb624cba754192b13be3f728fc860f
ReDay Zarra <redayzarra@gmail.com>
2024-07-23 04:42:48 -0400

    Adding better emails for verification

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import VerificationEmail from "@/components/emails/verificationEmail";
+
 const SettingsPage = () => {
   return (
     <div className="flex items-center justify-center">
-      sEttings
+      <VerificationEmail confirmLink="youtube.com" />
     </div>
   );
 };
--- lib/mail.ts
+++ lib/mail.ts
@@ -1,22 +1,21 @@
 import { Resend } from "resend";
+import VerificationEmail from "@/components/emails/verificationEmail";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/new-verification?token=${token}`;
-
   await resend.emails.send({
     from: "Welcome@studydsa.com",
     to: email,
     subject: "StudyDSA: Verify Your Email Address",
-    html: `<p>Click <a href=${confirmLink}>here</a> to verify your email.</p>`,
+    react: VerificationEmail({ confirmLink }),
   });
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/new-password?token=${token}`;
-
   await resend.emails.send({
     from: "Reset@studydsa.com",
     to: email,
