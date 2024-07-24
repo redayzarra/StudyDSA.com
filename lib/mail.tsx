a182ac945d49f0b8371febc3a8d01d580ec53874
ReDay Zarra <redayzarra@gmail.com>
2024-07-24 03:27:43 -0400

    Changing email address again since it's not sending

--- lib/mail.tsx
+++ lib/mail.tsx
@@ -7,7 +7,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/new-verification?token=${token}`;
   await resend.emails.send({
-    from: "Welcome@StudyDSA.com",
+    from: "Welcome@studydsa.com",
     to: email,
     subject: "StudyDSA: Verify Your Email Address",
     react: <VerificationEmail confirmLink={confirmLink} />,
@@ -17,7 +17,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/new-password?token=${token}`;
   await resend.emails.send({
-    from: "Reset@StudyDSA.com",
+    from: "Reset@studydsa.com",
     to: email,
     subject: "StudyDSA: Reset Your Password",
     html: `<p>Click <a href=${resetLink}>here</a> to reset your password.</p>`,
