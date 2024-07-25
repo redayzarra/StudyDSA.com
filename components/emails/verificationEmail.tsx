aa1abc34b390488b0ac547b52b736ffe51d8d0b7
ReDay Zarra <redayzarra@gmail.com>
2024-07-24 20:03:09 -0400

    Trying to fix the Resend emails

--- components/emails/verificationEmail.tsx
+++ components/emails/verificationEmail.tsx
@@ -10,72 +10,69 @@ import {
   Section,
   Text,
 } from "@react-email/components";
+import * as React from "react";
 
 interface Props {
   confirmLink: string;
 }
 
-const VerificationEmail = ({ confirmLink }: Props) => {
-  return (
-    <Html>
-      <Head>
-        <Font
-          fontFamily="Poppins"
-          fallbackFontFamily="Arial"
-          webFont={{
-            url: "https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJfecnFHGPc.woff2",
-            format: "woff2",
-          }}
-          fontWeight={400}
-          fontStyle="normal"
-        />
-        <Font
-          fontFamily="Poppins"
-          fallbackFontFamily="Arial"
-          webFont={{
-            url: "https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2",
-            format: "woff2",
-          }}
-          fontWeight={700}
-          fontStyle="normal"
-        />
-      </Head>
-      <Preview>Verify Your Email Address for StudyDSA</Preview>
-      <Body style={main}>
-        <Container style={container}>
-          <Section style={backgroundImage}>
-            <div style={logoContainer}>
-              <Img
-                src="/images/icon.png"
-                width={26}
-                height="auto"
-                alt="StudyDSA Logo"
-              />
-              <Text style={logoText}>StudyDSA</Text>
-            </div>
-            <Text style={heading}>Verify Your Email Address</Text>
-            <Text style={paragraph}>
-              Welcome to StudyDSA! We&apos;re excited to have you on board. To
-              get started, please verify your email address by clicking the
-              button below:
-            </Text>
-            <Section style={buttonContainer}>
-              <Link href={confirmLink} style={button}>
-                Verify Email
-              </Link>
-            </Section>
-            <Text style={footer}>
-              If you didn&apos;t create an account with StudyDSA, please ignore
-              this email.
-            </Text>
+export const VerificationEmail: React.FC<Readonly<Props>> = ({ confirmLink }) => (
+  <Html>
+    <Head>
+      <Font
+        fontFamily="Poppins"
+        fallbackFontFamily="Arial"
+        webFont={{
+          url: "https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJfecnFHGPc.woff2",
+          format: "woff2",
+        }}
+        fontWeight={400}
+        fontStyle="normal"
+      />
+      <Font
+        fontFamily="Poppins"
+        fallbackFontFamily="Arial"
+        webFont={{
+          url: "https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2",
+          format: "woff2",
+        }}
+        fontWeight={700}
+        fontStyle="normal"
+      />
+    </Head>
+    <Preview>Verify Your Email Address for StudyDSA</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Section style={backgroundImage}>
+          <div style={logoContainer}>
+            <Img
+              src="/images/icon.png"
+              width={26}
+              height="auto"
+              alt="StudyDSA Logo"
+            />
+            <Text style={logoText}>StudyDSA</Text>
+          </div>
+          <Text style={heading}>Verify Your Email Address</Text>
+          <Text style={paragraph}>
+            Welcome to StudyDSA! We&apos;re excited to have you on board. To get
+            started, please verify your email address by clicking the button
+            below:
+          </Text>
+          <Section style={buttonContainer}>
+            <Link href={confirmLink} style={button}>
+              Verify Email
+            </Link>
           </Section>
-        </Container>
-      </Body>
-    </Html>
-  );
-};
-
-export default VerificationEmail;
+          <Text style={footer}>
+            If you didn&apos;t create an account with StudyDSA, please ignore
+            this email.
+          </Text>
+        </Section>
+      </Container>
+    </Body>
+  </Html>
+);
 
 const fontFamily =
   "Poppins, Arial, &apos;Helvetica Neue&apos;, Helvetica, sans-serif";
